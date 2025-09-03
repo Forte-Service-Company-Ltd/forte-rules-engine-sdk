0f24fd92b503569b016ae9cee463b8d0ba74e236
Robert Kotz <robertkotz@macbookpro.lan>
2025-09-03 10:07:34 -1000

    prettier configs

---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  quoteProps: 'consistent',
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+}
+
