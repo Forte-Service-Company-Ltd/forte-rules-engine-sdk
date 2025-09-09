6ceb66645e92aafd24d23dae5ad0ff0e13094102
Michael Peterson <michaelpeterson@michaels-mbp.myfiosgateway.com>
2025-09-09 15:39:23 -0400

    Updated to point to Rules Engine 0.4.0

--- src/modules/types.ts
+++ src/modules/types.ts
@@ -1,11 +1,11 @@
 /// SPDX-License-Identifier: BUSL-1.1
 import { Address, BlockNumber, BlockTag, ByteArray, GetContractReturnType, Hex } from 'viem'
 
-// import RulesEnginePolicyLogicArtifact from '@fortefoundation/forte-rules-engine/out/RulesEnginePolicyFacet.sol/RulesEnginePolicyFacet.json'
-// import RulesEngineComponentLogicArtifact from '@fortefoundation/forte-rules-engine/out/RulesEngineComponentFacet.sol/RulesEngineComponentFacet.json'
-// import RulesEngineRuleLogicArtifact from '@fortefoundation/forte-rules-engine/out/RulesEngineRuleFacet.sol/RulesEngineRuleFacet.json'
-// import RulesEngineAdminLogicArtifact from '@fortefoundation/forte-rules-engine/out/RulesEngineAdminRolesFacet.sol/RulesEngineAdminRolesFacet.json'
-// import RulesEngineForeignCallLogicArtifact from '@fortefoundation/forte-rules-engine/out/RulesEngineForeignCallFacet.sol/RulesEngineForeignCallFacet.json'
+import RulesEnginePolicyLogicArtifact from '@fortefoundation/forte-rules-engine/out/RulesEnginePolicyFacet.sol/RulesEnginePolicyFacet.json'
+import RulesEngineComponentLogicArtifact from '@fortefoundation/forte-rules-engine/out/RulesEngineComponentFacet.sol/RulesEngineComponentFacet.json'
+import RulesEngineRuleLogicArtifact from '@fortefoundation/forte-rules-engine/out/RulesEngineRuleFacet.sol/RulesEngineRuleFacet.json'
+import RulesEngineAdminLogicArtifact from '@fortefoundation/forte-rules-engine/out/RulesEngineAdminRolesFacet.sol/RulesEngineAdminRolesFacet.json'
+import RulesEngineForeignCallLogicArtifact from '@fortefoundation/forte-rules-engine/out/RulesEngineForeignCallFacet.sol/RulesEngineForeignCallFacet.json'
 import {
   CallingFunctionJSON,
   ForeignCallJSON,
@@ -14,12 +14,6 @@ import {
   RuleJSON,
   TrackerJSON,
 } from './validation'
-
-import RulesEnginePolicyLogicArtifact from "../../abis/RulesEnginePolicyFacet.json"
-import RulesEngineComponentLogicArtifact from "../../abis/RulesEngineComponentFacet.json"
-import RulesEngineRuleLogicArtifact from "../../abis/RulesEngineRuleFacet.json"
-import RulesEngineAdminLogicArtifact from "../../abis/RulesEngineAdminRolesFacet.json"
-import RulesEngineForeignCallLogicArtifact from "../../abis/RulesEngineForeignCallFacet.json"
 
 /**
  * @file types.ts
